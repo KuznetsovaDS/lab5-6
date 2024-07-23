3cc8099862e8c0dddc238b37e19de0aa7edc28d0
Darya Kuznetsova <Dashar1996@gmail.com>
2024-07-24 00:08:13 +0300

    Add files via upload

---
+++ lab6_task1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    system("chcp 1251");
+    ofstream out("poem.txt");
+    if (!out) {
+        cout << "невозможно открыть файл для записи";
+        return 1;
+    }
+
+    string line;
+    cout << "Введите стихотворение " << endl;
+    while (true) {
+        getline(cin, line);
+        if (line.empty()) break; 
+        out << line << endl;
+    }
+    out.close();
+
+    ifstream in("poem.txt");
+    if (!in) {
+        cout << "невозможно открыть файл для чтения\n";
+        return 1;
+    }
+
+    cout << "\ncодержимое файла " << endl;
+    while (getline(in, line)) {
+        cout << line << endl;
+    }
+
+    in.close();
+
+}
